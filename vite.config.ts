a70eefa0fd8ed36a383f1a83bf04774a530e1ca7
nce40202 <nce40202@ly.com>
2025-02-19 10:56:17 +0800

    feat: 配置生产环境的基础路径

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/newbie-tix-manager/" : "/",
   plugins: [
     vue(),
     vueDevTools(),
